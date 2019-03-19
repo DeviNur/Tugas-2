2edb9fec9e4914e50c558189ff5d4437255ea2c7
DeviNur <41379185+DeviNur@users.noreply.github.com>
2019-03-19 13:21:13 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    
+    $kon = mysqli_connect("localhost","id7899377_devi","devinurama123","id7899377_akademik");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
